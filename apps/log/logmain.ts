eb7ca1380d3edc6d03a5c4dcbf3dd9a9684622b8
Emirhan0841 <emirhan.yasa@outlook.de>
2025-11-12 21:57:56 +0100

    refactor(log): Startlogik in logmain.ts überarbeitet

--- apps/log/logmain.ts
+++ apps/log/logmain.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { LogModule } from './log-module';
+import { LogModule } from './LogModule';
 import { Transport, RmqOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
+  //* Erstellt den Log-Microservice
   const app = await NestFactory.createMicroservice<RmqOptions>(LogModule, {
     transport: Transport.RMQ,
     options: {
       urls: [process.env.AMQP_URL || 'amqp://guest:guest@127.0.0.1:5672'],
-      //* Hier wird der Kanal für den Log-Service definiert
+      //* Hier wird der Kanal definiert, auf den der Log-Service hört
       queue: 'log_queue',
       queueOptions: {
         durable: true, //
