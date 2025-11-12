c1227b9a4b7e1df7406ce91f02962f0b33b1fcd5
Emirhan0841 <emirhan.yasa@outlook.de>
2025-11-12 21:57:16 +0100

    feat(inventory): InventoryModule angepasst

--- apps/inventory/InventoryModule.ts
+++ apps/inventory/InventoryModule.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { InventoryController } from './InventoryController';
 import { InventoryService } from './InventoryService';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'LOG_CLIENT',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.AMQP_URL || 'amqp://guest:guest@127.0.0.1:5672'],
+          queue: 'log_queue',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [InventoryController],
   providers: [InventoryService],
 })
