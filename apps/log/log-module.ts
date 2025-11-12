26bf24e4bb364d0ef47e94c34a77ddd49f79023d
Emirhan0841 <emirhan.yasa@outlook.de>
2025-11-12 22:01:55 +0100

    LogService angepasst.

--- apps/log/log-module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LogService } from './log-service';
-
-@Module({
-  imports: [],
-  providers: [],
-  controllers: [LogService],
-})
-export class LogModule {}
